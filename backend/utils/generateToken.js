27ee6eeaa053862595546414bb88c53731c4fdd1
Swaitz Joses <swaitz.joses@gmail.com>
2021-05-28 10:10:13 +0530

    first commit

--- backend/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken'
-
-const generateToken=(id, name)=>{ // id is the payload  
-    return jwt.sign({id, name}, process.env.JWT_SECRET , {
-        expiresIn:'30d',
-    })
-}
-
-export default generateToken
